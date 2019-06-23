71f310fa776f3deaced35878036cd64ba3ea5795
goadelaja2002 <goadelaja2002@gmail.com>
2019-06-23 21:00:56 +0100

    Initial commit including index,css and javascript

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+function myScript(){
+	var x,y;
+	x = document.getElementById("myinfo");
+	y = x.elements["nametest"].value;
+	document.getElementById("cont").innerHTML = "<h1>" + y + "</h>";
+
+}
